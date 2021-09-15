b484bebb306699ba53f39a7043bb7c1bd6142dc0
Stefan Koch <uplink.coder@googlemail.com>
2021-09-15 07:51:09 +0200

    Update submodule

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-dmd bc_lightning_backend.d bc_interpreter_backend.d bc_common.d fpconv_ctfe.d bc_abi.d bc_limits.d lightning-d/lightning.d lightning-d/jit_x86_64.d -L-llightning -L-L/usr/local/lib -g examples.d
+ldmd2 bc_lightning_backend.d bc_interpreter_backend.d bc_common.d fpconv_ctfe.d bc_abi.d bc_limits.d \
+lightning-d/lightning.d \
+lightning-d/jit_aarch64.d \
+lightning-d/jit_x86_64.d \
+-L-llightning -L-L/usr/local/lib -g examples.d
