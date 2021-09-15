dd041af23e4e677ce3f0563053dda77cb8b6530a
Stefan Koch <uplink.coder@googlemail.com>
2021-09-15 07:10:51 +0200

    Add examples.d

---
+++ examples.d
@@ -0,0 +1,47 @@
+import dmd.ctfe.bc_common;
+import dmd.ctfe.bc;
+// Fact(n) = (n > 1 ? ( n*(n-1) * Fact(n-2) ) : 1);
+auto testFact(GenT)(GenT gen)
+{
+    with (gen)
+    {
+        Initialize();
+        {
+            auto n = genParameter(i32Type, "n");
+            beginFunction(0);
+            {
+                auto result = genLocal(i32Type, "result");
+                Gt3(BCValue.init, n, imm32(1, true));
+                auto j_n_lt_1 = beginCndJmp();
+                {
+                    auto n_sub_1 = genTemporary(i32Type);
+                    Sub3(n_sub_1, n, imm32(1));
+                    auto n_mul_n_sub_1 = genTemporary(i32Type);
+                    Mul3(n_mul_n_sub_1, n, n_sub_1);
+                    Sub3(n, n, imm32(2));
+
+                    auto result_fact = genTemporary(i32Type);
+                    Call(result_fact, imm32(1), [n]);
+
+                    Mul3(result, n_mul_n_sub_1, result_fact);
+                    Ret(result);
+                }
+                auto l_n_lt_1 = genLabel();
+                {
+                    Set(result, imm32(1));
+                    Ret(result);
+                }
+                endCndJmp(j_n_lt_1, l_n_lt_1);
+            }
+            endFunction();
+        }
+        Finalize();
+        return gen;
+    }
+}
+static assert(testFact(BCGen.init).interpret([imm32(5)]) == imm32(120));
+
+void main()
+{
+ 
+}
